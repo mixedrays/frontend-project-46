0f3041d00e2d2f53140fba06181063639d646008
Aleks M <aleksm@Alekss-MacBook-Pro.local>
2023-01-03 10:58:40 +0100

    Add format option

--- gendiff.js
+++ gendiff.js
@@ -8,4 +8,7 @@ program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0');
 
+program
+    .option('-f, --format', 'output format')
+
 program.parse();
